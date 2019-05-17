b2663b960dffbb3c5cda1f1c0fa497a3804df70c
sizigia <cumbresdigitales@gmail.com>
2019-05-17 20:40:12 -0300

    add method to delete articles and get back to the articles listing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,4 +18,10 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
 end

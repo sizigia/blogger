8eaa36414595688222bbf3ad7198d0705acc23be
sizigia <cumbresdigitales@gmail.com>
2019-05-18 20:23:57 -0300

    add the option to delete a tag

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -8,4 +8,11 @@ class TagsController < ApplicationController
   def show
     @tag = Tag.find(params[:id])
   end
+
+  def destroy
+    @tag = Tag.find(params[:id])
+    @tag.destroy
+
+    redirect_to tags_path
+  end
 end

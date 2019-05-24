cd7ec343fc1404f1b1bc3d9a16209558e4fd79bb
sizigia <cumbresdigitales@gmail.com>
2019-05-23 22:56:16 -0300

    styling

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,6 +11,8 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.precompile += %w[reset.css]
 Rails.application.config.assets.precompile += %w[top-navbar.css]
+Rails.application.config.assets.precompile += %w[fonts.css]
+Rails.application.config.assets.precompile += %w[colors.css]
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
